ea0b1fdab674a0b711820451f2d87fc8ec54d54a
yohei-kobashi <yohei.kobashi@gmail.com>
2025-06-27 04:35:07 +0900

    miyabi batch script

---
+++ extract_test_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#------ qsub option --------#
+#PBS -q small-c
+#PBS -l select=1
+#PBS -l walltime=24:00:00
+#PBS -W group_list=gj26
+#PBS -j oe
+
+#------- Program execution -------#
+module purge
+module load cmake
+module load gcc
+
+cd crypto_llm
+source env_crypto_llm/bin/activate
+python script/preprocess/extract_name.py extract_names_from_fwe100b-10b
