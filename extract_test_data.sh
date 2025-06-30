9aeddd626e963ca9b27505d3482f02e4af13411a
yohei-kobashi <yohei.kobashi@gmail.com>
2025-06-30 14:28:52 +0900

    misc

--- extract_test_data.sh
+++ extract_test_data.sh
@@ -14,6 +14,6 @@ module load gcc
 
 cd crypto_llm
 source env_crypto_llm/bin/activate
-python script/preprocess/parquet_sampler.py --input-dir fineweb-edu/sample/100BT-10BT --output-dir fineweb-edu/sample/100BT-10BT-sampled2 --sampling-rate 1/9 --n_tasks 20
+python script/preprocess/parquet_sampler.py --input-dir fineweb-edu/sample/100BT-10BT --output-dir fineweb-edu/sample/100BT-10BT-sampled2 --sampling-rate 1/9 --num-procs 10
 python script/preprocess/extract_name.py fineweb-edu/sample/100BT-10BT-sampled2 extract_names_from_fwe100b-10b_2 --n_tasks 20
 python script/preprocess/filter_name.py fineweb-edu/sample/100BT-10BT-sampled2 extract_names_from_fwe100b-10b_2 --n_tasks 20
