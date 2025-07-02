1112b0f1acdaf55138a1876f212aca80836a7e42
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-03 00:52:06 +0900

    update suffix array sh

--- suffix_array_search.sh
+++ suffix_array_search.sh
@@ -14,4 +14,10 @@ module load gcc
 
 cd crypto_llm
 source env_crypto_llm/bin/activate
-python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT --workers 14
+# python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT --workers 14
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 35
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 30
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 25
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 20
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 15
+python script/preprocess/suffix_array_search.py build --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 10
