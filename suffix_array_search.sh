d428677957ef2ae1fbdb7de49bd1502e51319f44
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-02 16:14:38 +0900

    update suffix array sh

--- suffix_array_search.sh
+++ suffix_array_search.sh
@@ -14,4 +14,4 @@ module load gcc
 
 cd crypto_llm
 source env_crypto_llm/bin/activate
-python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-split --out-dir fineweb-edu/index_10BT
+python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT
