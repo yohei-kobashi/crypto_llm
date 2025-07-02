e3107937c7532189c650bbde5b3a6f69e6e835c1
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-03 03:38:49 +0900

    update suffix array sh

--- suffix_array_search.sh
+++ suffix_array_search.sh
@@ -15,9 +15,8 @@ module load gcc
 cd crypto_llm
 source env_crypto_llm/bin/activate
 # python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT --workers 14
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 35 --workers 2
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 30 --workers 2
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 25 --workers 2
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 20 --workers 2
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 15 --workers 2
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/stream_generation_output_20250702_033516.jsonl --window 10 --workers 2
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 25
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 20
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 15
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 10
