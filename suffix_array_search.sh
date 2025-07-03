efb8ae254ffff768dd3ec0fa633566165c878750
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-03 13:15:22 +0900

    modify miyabi sh

--- suffix_array_search.sh
+++ suffix_array_search.sh
@@ -15,8 +15,10 @@ module load gcc
 cd crypto_llm
 source env_crypto_llm/bin/activate
 # python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT --workers 14
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 35
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 25
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 20
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 15
-python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/split --window 10
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_1 --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_10 --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_100 --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/b --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/c --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/d --window 35
