7351b134bf5caa83d6ec91022c058fa590164f76
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-03 15:00:14 +0900

    add new sh

---
+++ suffix_array_search3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#------ qsub option --------#
+#PBS -q regular-c
+#PBS -l select=1
+#PBS -l walltime=48:00:00
+#PBS -W group_list=gj26
+#PBS -j oe
+
+#------- Program execution -------#
+module purge
+module load cmake
+module load gcc
+
+cd crypto_llm
+source env_crypto_llm/bin/activate
+# python script/preprocess/suffix_array_search.py build --parquet-dir fineweb-edu/sample/10BT-sample --out-dir fineweb-edu/index_10BT --workers 14
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/e/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_1/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_10/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/a_100/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/b/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/c/1000.jsonl --window 35
+python script/preprocess/suffix_array_search.py query --index-dir fineweb-edu/index_10BT --input output/d/1000.jsonl --window 35
