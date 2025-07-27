32dab24a9cce14076abbf370f5858860e7ab591c
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-27 10:39:06 +0900

    misc

--- estimate_preprocessing_speed.py
+++ estimate_preprocessing_speed.py
@@ -1,18 +1,19 @@
 import timeit
 import numpy as np
+import random
 
 import pyarrow.parquet as pq
 from script.preprocess import encrypt_alpha
 from script.preprocess.scrub_jsonl import scrubbing
 
 
-def encryption(texts):
+def encrypting_text(texts):
     encryptor = encrypt_alpha.Encryptor()
     encryptor.poly(100, lang="alpha", reuse_key=True)
     for text in texts:
         encryptor.encrypt(text)
 
-def scrubbing(texts):
+def scrubbing_texts(texts):
     for text in texts:
         scrubbing(text)
 
@@ -20,12 +21,13 @@ def main():
     dataset = pq.ParquetDataset("fineweb-edu/sample/10BT-sample/")
     texts = dataset.read(columns=["text"])
     texts = texts["text"].to_numpy()
+    samples = random.sample(list(texts), 1000)
 
-    enc_timer = timeit.Timer(lambda: encryption(texts))
+    enc_timer = timeit.Timer(lambda: encrypting_text(samples))
     times = enc_timer.repeat(repeat=10, number=1)
     print("encryption:", np.mean(times), np.std(times))
     
-    scrub_timer = timeit.Timer(lambda: scrubbing(texts))
+    scrub_timer = timeit.Timer(lambda: scrubbing_texts(samples))
     times = scrub_timer.repeat(repeat=10, number=1)
     print("scrubbing:", np.mean(times), np.std(times))
 
