227a3b3ebf1a68a92242c45a2d12bcd657271ddf
yohei-kobashi <yohei.kobashi@gmail.com>
2025-07-27 12:03:21 +0900

    misc

--- estimate_preprocessing_speed.py
+++ estimate_preprocessing_speed.py
@@ -5,6 +5,7 @@ import random
 import pyarrow.parquet as pq
 from script.preprocess import encrypt_alpha
 from script.preprocess.scrub_jsonl import scrubbing
+from tqdm import tqdm
 
 
 def encrypting_text(texts):
@@ -14,22 +15,26 @@ def encrypting_text(texts):
         encryptor.encrypt(text)
 
 def scrubbing_texts(texts):
-    for text in texts:
+    for text in tqdm(texts):
         scrubbing(text)
 
 def main():
     dataset = pq.ParquetDataset("fineweb-edu/sample/10BT-sample/")
     texts = dataset.read(columns=["text"])
     texts = texts["text"].to_numpy()
-    samples = random.sample(list(texts), 1000)
+    enc_times = []
+    scrub_times = []
+    for _ in range(10):
+        samples = random.sample(list(texts), 1000)
 
-    enc_timer = timeit.Timer(lambda: encrypting_text(samples))
-    times = enc_timer.repeat(repeat=10, number=1)
-    print("encryption:", np.mean(times), np.std(times))
-    
-    scrub_timer = timeit.Timer(lambda: scrubbing_texts(samples))
-    times = scrub_timer.repeat(repeat=10, number=1)
-    print("scrubbing:", np.mean(times), np.std(times))
+        enc_timer = timeit.Timer(lambda: encrypting_text(samples))
+        enc_times.append(enc_timer.timeit(number=1))
+        
+        scrub_timer = timeit.Timer(lambda: scrubbing_texts(samples))
+        scrub_times.append(scrub_timer.timeit(number=1))
+        
+    print("encryption:", np.mean(enc_times), np.std(enc_times))
+    print("scrubbing:", np.mean(scrub_times), np.std(scrub_times))
 
 if __name__ == "__main__":
     main()
